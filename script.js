eab8159782619f842f363897166e8c4cdf04cddc
pc_home <m.mazouz67@gmail.com>
2020-10-11 18:57:40 +0200

    all

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+var navbar = document.getElementById("header");
+var img = document.getElementById("top");
+var sticky = img.offsetTop;
+
 window.onscroll = function() {
-{
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    navbar.classList.remove("nosticky")
+    navbar.classList.add("text-primary")
+
   } else {
     navbar.classList.remove("sticky");
+    navbar.classList.add("nosticky")
+    navbar.classList.remove("text-primary")
   }
-}
 };
-var navbar = document.getElementById("header");
-var sticky = navbar.offsetTop;
+
